6fc798504ee1e75088ea826ff59ed70f496c76af
railken <railken@protonmail.ch>
2019-05-11 17:08:41 +0200

    fix style

--- src/Models/Token.php
+++ src/Models/Token.php
@@ -14,7 +14,8 @@ use Railken\Lem\Contracts\EntityContract;
  */
 class Token extends Model implements EntityContract
 {
-    use SoftDeletes, ConfigurableModel;
+    use SoftDeletes;
+    use ConfigurableModel;
 
     /**
      * Create a new Eloquent model instance.
