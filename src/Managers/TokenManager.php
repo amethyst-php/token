5a2e3b730abcc0a1e71cddad5029c7c2c10dacb8
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/TokenManager.php
+++ src/Managers/TokenManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Token newEntity()
+ * @method \Amethyst\Schemas\TokenSchema getSchema()
+ * @method \Amethyst\Repositories\TokenRepository getRepository()
+ * @method \Amethyst\Serializers\TokenSerializer getSerializer()
+ * @method \Amethyst\Validators\TokenValidator getValidator()
+ * @method \Amethyst\Authorizers\TokenAuthorizer getAuthorizer()
+ */
 class TokenManager extends Manager
 {
     use ConfigurableManager;
--- src/Managers/TokenTypeManager.php
+++ src/Managers/TokenTypeManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\TokenType newEntity()
+ * @method \Amethyst\Schemas\TokenTypeSchema getSchema()
+ * @method \Amethyst\Repositories\TokenTypeRepository getRepository()
+ * @method \Amethyst\Serializers\TokenTypeSerializer getSerializer()
+ * @method \Amethyst\Validators\TokenTypeValidator getValidator()
+ * @method \Amethyst\Authorizers\TokenTypeAuthorizer getAuthorizer()
+ */
 class TokenTypeManager extends Manager
 {
     use ConfigurableManager;
